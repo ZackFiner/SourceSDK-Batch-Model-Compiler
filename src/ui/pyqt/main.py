8cfbaf09743d69c0afeba5c27a8247ffc08248c8
ZackFiner <zackary.finer@yahoo.com>
2021-01-26 18:10:47 -0800

    Update main.py

--- src/ui/pyqt/main.py
+++ src/ui/pyqt/main.py
@@ -1,6 +1,16 @@
 from PyQt5.QtWidgets import *
 import sys
 
+
+def put_file_in_dict(parent, text_dict, keyword):
+    def func():
+        path, _ = QFileDialog.getOpenFileName(parent, 'Select File')
+        if keyword in text_dict:
+            text_dict[keyword].setText(path)
+
+    return func
+
+
 class SMDSelector(QWidget):
     def __init__(self, *args, **kwargs):
         # pop any custom arguments up here
@@ -21,11 +31,11 @@ class SMDSelector(QWidget):
         file_path_layout.addWidget(self.file_paths['ref_smd'], 2, 1)
 
         self.btn_select_mesh_smd = QPushButton('...')
-        self.btn_select_mesh_smd.clicked.connect(self.get_file_path_from_dialog('mesh_smd'))
+        self.btn_select_mesh_smd.clicked.connect(put_file_in_dict(self, self.file_paths, 'mesh_smd'))
         self.btn_select_phys_smd = QPushButton('...')
-        self.btn_select_phys_smd.clicked.connect(self.get_file_path_from_dialog('phys_smd'))
+        self.btn_select_phys_smd.clicked.connect(put_file_in_dict(self, self.file_paths, 'phys_smd'))
         self.btn_select_ref_smd = QPushButton('...')
-        self.btn_select_ref_smd.clicked.connect(self.get_file_path_from_dialog('ref_smd'))
+        self.btn_select_ref_smd.clicked.connect(put_file_in_dict(self, self.file_paths, 'ref_smd'))
         file_path_layout.addWidget(self.btn_select_mesh_smd, 0, 2)
         file_path_layout.addWidget(self.btn_select_phys_smd, 1, 2)
         file_path_layout.addWidget(self.btn_select_ref_smd, 2, 2)
@@ -36,12 +46,29 @@ class SMDSelector(QWidget):
         main_layout.addWidget(self.file_path_area)
         self.setLayout(main_layout)
 
-    def get_file_path_from_dialog(self, key_word):
-        def func():
-            path, _ = QFileDialog.getOpenFileName(self, 'Select File')
-            if key_word in self.file_paths:
-                self.file_paths[key_word].setText(path)
-        return func
+
+class MdlSelector(QWidget):
+    def __init__(self, *args, **kwargs):
+        super(MdlSelector, self).__init__(*args, **kwargs)
+        self.file_paths = dict()
+        self.file_path_area = QWidget()
+        file_path_layout = QGridLayout()
+
+        file_path_layout.addWidget(QLabel('Model MDL'), 0, 0)
+
+        self.file_paths['model_mdl'] = QLineEdit()
+        file_path_layout.addWidget(self.file_paths['model_mdl'], 0, 1)
+
+        self.btn_select_mdl = QPushButton('...')
+        self.btn_select_mdl.clicked.connect(put_file_in_dict(self, self.file_paths, 'model_mdl'))
+        file_path_layout.addWidget(self.btn_select_mdl, 0, 2)
+
+        self.file_path_area.setLayout(file_path_layout)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.file_path_area)
+        self.setLayout(main_layout)
+
 
 
 class MainWindow(QMainWindow):
@@ -56,13 +83,15 @@ class MainWindow(QMainWindow):
         self.btn.clicked.connect(self.do_something)
 
         layout = QGridLayout()
-
-        layout.addWidget(SMDSelector(), 0, 0)
+        smd_selection_tabs = QTabWidget()
+        self.smd_selector = smd_selection_tabs
+        smd_selection_tabs.addTab(SMDSelector(), 'Select SMD Files')
+        smd_selection_tabs.addTab(MdlSelector(), 'Select Mdl File')
+        layout.addWidget(smd_selection_tabs, 0, 0)
         self.cw.setLayout(layout)
 
     def do_something(self):
-        self.index += 1
-        print(self.index)
+        print(self.smd_selector.currentIndex())
 
     def closeEvent(self, e):
         pass
